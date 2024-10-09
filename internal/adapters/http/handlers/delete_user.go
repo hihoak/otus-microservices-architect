67248ab9e0caf1c0885732b0062b43708add2b25
artemikhaylov <artemikhaylov@ozon.ru>
2024-10-10 00:43:24 +0300

    Add auth-service + add auth + authorization
    This reverts commit fd1ca6728f5e920c21f384f19a5cef450de49f96.

--- internal/adapters/http/handlers/delete_user.go
+++ internal/adapters/http/handlers/delete_user.go
@@ -13,15 +13,17 @@ import (
 func (s Service) DeleteUserHandler(c *gin.Context) {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("wrong id format it must be int: %s", err.Error())})
+		c.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("wrong id format it must be int: %s", err.Error())})
+		return
 	}
-
-	err = s.usersService.DeleteUser(context.Background(), uint64(id))
+	username, _, _ := c.Request.BasicAuth()
+	err = s.usersService.DeleteUser(context.Background(), uint64(id), username)
 	if err != nil {
 		if errors.Is(err, user.ErrNotFound) {
 			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"error": err.Error()})
 		}
-		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
 	}
 
 	c.JSON(http.StatusOK, gin.H{})
